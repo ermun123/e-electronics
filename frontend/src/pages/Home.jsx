122b49b6654a89a94369a2a69bfde0c5234c50a8
ermun123 <oyunbayarermun@gmail.com>
2023-03-24 01:24:25 +0800

    bajigaa

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
+import MostSection from "../components/MostSection";
 
 export default function Home() {
     return (
         <>
             <Navbar/>
+            <MostSection/>
         </>
     )
 }
